9b423406025929f9ffdf24fbedd7a5bf6150e82b
Mehdi El <mehdielallam07@gmail.com>
2021-03-01 23:16:40 +0100

    Mise à jour migration

--- migrations/Version20210227145202.php
+++ migrations/Version20210227145202.php
@@ -21,7 +21,6 @@ final class Version20210227145202 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(60) NOT NULL, prenom VARCHAR(60) NOT NULL, matricule VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D64912B2DC9C (matricule), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('INSERT INTO user(nom, prenom, matricule, password, roles) VALUES("Kalitics", "INC", "kalitics" , "$argon2id$v=19$m=65536,t=4,p=1$YnFmajZzeGlVa1NzMjczTQ$i9aT7XmtB+VmfjgBz2U8Y4Qp6HH/+kw8seWfdYR/ywc", "["ROLE_ADMIN"]") ');
     }
 
     public function down(Schema $schema) : void
